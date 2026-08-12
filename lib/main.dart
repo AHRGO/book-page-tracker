d14cd5a2db0c7abb0c46c605b8fac0eb2e8aaabd
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2026-08-12 21:27:12 +0000

    starts to experiment with hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 
 import 'presentation/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  await Hive.openBox('trackers'); //? actual 'table' name?
+
   runApp(const MyApp());
 }
 
@@ -21,3 +28,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+///
+/// todo: play with hive a little to see how it behaves
+///   final box = Hive.box('users');
+///   
+///   await box.put('name', 'Victor');
+///   await box.put('age', 28);
+///   
+///   print(box.get('name'));
+///   print(box.get('age'));
+///
+///   await box.delete('age');
+///
